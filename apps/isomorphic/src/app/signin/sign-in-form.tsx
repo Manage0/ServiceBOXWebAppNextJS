8dcf8dcd08ae77815ef3a72644e789cc19330625
Manage0 <jkbbenedek@gmail.com>
2024-12-14 20:44:59 +0100

    forgotPassword1 reference changed to forgotPassword3

--- apps/isomorphic/src/app/signin/sign-in-form.tsx
+++ apps/isomorphic/src/app/signin/sign-in-form.tsx
@@ -65,7 +65,7 @@ export default function SignInForm() {
                 className="[&>label>span]:font-medium"
               />
               <Link
-                href={routes.auth.forgotPassword1}
+                href={routes.auth.forgotPassword3}
                 className="h-auto p-0 text-sm font-semibold text-blue underline transition-colors hover:text-gray-900 hover:no-underline"
               >
                 Forget Password?
