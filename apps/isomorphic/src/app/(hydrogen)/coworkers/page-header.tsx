eb02244906b0d3e235a0f3a3e6f02114c8400c7f
Manage0 <jkbbenedek@gmail.com>
2025-01-09 09:50:01 +0100

    Import btn removed from Coworkers

--- apps/isomorphic/src/app/(hydrogen)/coworkers/page-header.tsx
+++ apps/isomorphic/src/app/(hydrogen)/coworkers/page-header.tsx
@@ -3,7 +3,6 @@
 import { PiPlusBold } from 'react-icons/pi';
 import { Button } from 'rizzui';
 import PageHeader from '@/app/shared/page-header';
-import ImportButton from '@/app/shared/import-button';
 import { useModal } from '@/app/shared/modal-views/use-modal';
 import AddTeamMemberModalView from '@/app/shared/account-settings/modal/add-team-member';
 
@@ -29,10 +28,6 @@ export default function CoworkersPageHeader() {
   return (
     <PageHeader title={pageHeader.title} breadcrumb={pageHeader.breadcrumb}>
       <div className="mt-4 flex flex-col items-center gap-3 @sm:flex-row @lg:mt-0">
-        <ImportButton
-          title="Upload File"
-          className="me-[10px] mt-4 border-0 border-[1px] border-[#E3E3E3] bg-white text-[#484848] hover:border-[#E3E3E3] hover:bg-gray-100 hover:text-[#484848] @lg:mt-0"
-        />
         <Button
           as="span"
           className="w-full @lg:w-auto"
