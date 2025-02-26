4e330e3832f9dd50b1b93e3438faa9c925af99e4
Manage0 <jkbbenedek@gmail.com>
2025-02-26 08:54:07 +0100

    0-24 options instead of 8-17

--- apps/isomorphic/src/app/shared/options.ts
+++ apps/isomorphic/src/app/shared/options.ts
@@ -48,6 +48,14 @@ export const priorityOptions: SelectOption[] = [
 ];
 
 export const timeOptions: SelectOption[] = [
+  { label: '00:00', value: '00:00:00' },
+  { label: '01:00', value: '01:00:00' },
+  { label: '02:00', value: '02:00:00' },
+  { label: '03:00', value: '03:00:00' },
+  { label: '04:00', value: '04:00:00' },
+  { label: '05:00', value: '05:00:00' },
+  { label: '06:00', value: '06:00:00' },
+  { label: '07:00', value: '07:00:00' },
   { label: '08:00', value: '08:00:00' },
   { label: '09:00', value: '09:00:00' },
   { label: '10:00', value: '10:00:00' },
@@ -58,4 +66,10 @@ export const timeOptions: SelectOption[] = [
   { label: '15:00', value: '15:00:00' },
   { label: '16:00', value: '16:00:00' },
   { label: '17:00', value: '17:00:00' },
+  { label: '18:00', value: '18:00:00' },
+  { label: '19:00', value: '19:00:00' },
+  { label: '20:00', value: '20:00:00' },
+  { label: '21:00', value: '21:00:00' },
+  { label: '22:00', value: '22:00:00' },
+  { label: '23:00', value: '23:00:00' },
 ];
